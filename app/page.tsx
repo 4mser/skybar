5a97bf7475b210362c114f3808ac6822d31d9880
4mser <nicolas.moreno.sagan@gmail.com>
2024-09-28 23:59:56 -0300

    test4

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,23 @@
 import Banner from "./components/banner";
 import Anuncios from "./components/anuncios";
 import Cartas from "./components/cartas";
-import BokehBackground from "./components/blackhole";
+
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const BokehBackground = dynamic(() => import('./components/blackhole'), {
+  ssr: false,
+});
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true); // Solo se monta en el cliente
+  }, []);
+
+  if (!isMounted) return null; // No renderizar nada en SSR
+
   return (
     <main>
       {/* <Topbar /> */}
