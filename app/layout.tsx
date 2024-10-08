40630654e5d52bb41d7b55edf995d1e9f692df5c
4mser <nicolas.moreno.sagan@gmail.com>
2024-10-08 14:39:51 -0300

    menu radial arreglado para los angulos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Topbar from "./components/topbar"; 
 import dynamic from "next/dynamic";
