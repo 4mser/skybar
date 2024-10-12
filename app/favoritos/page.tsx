dfd1f6123e81a12638e8c26605a4d742c44f890e
4mser <nicolas.moreno.sagan@gmail.com>
2024-10-12 18:41:46 -0300

    cambios favoritos

--- app/favoritos/page.tsx
+++ app/favoritos/page.tsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { FaHeart } from 'react-icons/fa';
 import { removeFavorite, getFavoriteProducts } from '../services/api';
 import Image from 'next/image';
+import { useDarkMode } from '../context/DarkModeContext'; // Importamos useDarkMode
 
 interface Product {
   _id?: string;
@@ -16,13 +17,37 @@ interface Product {
 }
 
 const FavoritesPage: React.FC = () => {
-  const [favoriteProducts, setFavoriteProducts] = useState<Product[]>([]); 
+  const [favoriteProducts, setFavoriteProducts] = useState<Product[]>([]);
+
+  // Obtenemos el modo de fondo desde el contexto
+  const { backgroundMode } = useDarkMode();
+
+  // Definimos las clases de fondo y texto según el modo de fondo
+  const backgroundClass =
+    backgroundMode === 'dark'
+      ? 'bg-[#0a0a0a]'
+      : backgroundMode === 'light'
+      ? ''
+      : 'bg-slate-100';
+
+  const textAndBorderClass =
+    backgroundMode === 'neon'
+      ? 'text-black/80 border-black/30'
+      : 'text-white border-white/20';
 
   // Cargar los productos favoritos al montar el componente
   useEffect(() => {
     const fetchFavorites = async () => {
       try {
         const favoriteProductsData = await getFavoriteProducts();
+
+        // Ordenar los productos para que los que tienen imagen aparezcan primero
+        favoriteProductsData.sort((a: Product, b: Product) => {
+          if (a.imageUrl && !b.imageUrl) return -1;
+          if (!a.imageUrl && b.imageUrl) return 1;
+          return 0;
+        });
+
         setFavoriteProducts(favoriteProductsData);
       } catch (error) {
         console.error('Error al obtener productos favoritos:', error);
@@ -36,7 +61,7 @@ const FavoritesPage: React.FC = () => {
   const toggleFavorite = async (productId: string) => {
     try {
       await removeFavorite(productId);
-      setFavoriteProducts(favoriteProducts.filter(product => product._id !== productId));
+      setFavoriteProducts(favoriteProducts.filter((product) => product._id !== productId));
     } catch (error) {
       console.error('Error al eliminar producto de favoritos:', error);
     }
@@ -50,9 +75,11 @@ const FavoritesPage: React.FC = () => {
 
   return (
     <>
-      <div className="pt-12">
+      <div className={`pt-12 ${backgroundClass}`}>
         {favoriteProducts.length === 0 ? (
-          <p className="text-center text-white">No tienes productos favoritos</p>
+          <p className={`text-center ${textAndBorderClass}`}>
+            
+          </p>
         ) : (
           <motion.div
             className="mt-4"
@@ -61,14 +88,14 @@ const FavoritesPage: React.FC = () => {
             transition={{ duration: 0.3 }}
             variants={variants}
           >
-            <h2 className="text-2xl font-semibold text-center ">
+            <h2 className={`text-2xl pt-3 font-semibold text-center ${textAndBorderClass}`}>
               Tus Productos Favoritos
             </h2>
             <ul className="text-xs flex flex-col mb-3">
               {favoriteProducts.map((product: Product, productIndex: number) => (
                 <li
                   key={productIndex}
-                  className="flex justify-between items-center px-4 py-1 mt-3 gap-8 modal-item cursor-pointer text-white border-white/20"
+                  className={`flex justify-between items-center px-4 py-1 mt-3 gap-8 modal-item cursor-pointer ${textAndBorderClass}`}
                 >
                   <div className="flex items-center">
                     {product.imageUrl && (
@@ -81,14 +108,20 @@ const FavoritesPage: React.FC = () => {
                       />
                     )}
                     <div>
-                      <h1 className="font-semibold">{product.name}</h1>
-                      <p className="font-normal opacity-70">{product.description}</p>
+                      <h1 className={`font-semibold ${textAndBorderClass}`}>
+                        {product.name}
+                      </h1>
+                      <p className={`font-normal opacity-70 ${textAndBorderClass}`}>
+                        {product.description}
+                      </p>
                     </div>
                   </div>
                   <div className="flex items-center">
-                    <div className="min-w-20 border px-2 rounded-[8px]">
-                      <p className="w-full text-sm text-center font-medium">
-                        ${product.price}
+                    <div className={`min-w-20 border px-2 rounded-[8px] ${textAndBorderClass}`}>
+                      <p
+                        className={`w-full text-sm text-center font-medium ${textAndBorderClass}`}
+                      >
+                        ${product.price.toLocaleString('es-CL')}
                       </p>
                     </div>
                     <div
